b111b401c661a0f29d94cab54f04cb1076b6d2ee
THIAGO RIVAS CABALLERO <thiago.rcaballero@senacsp.edu.br>
2024-03-05 09:55:15 -0300

    pega ai caio

--- primeiro-app/src/components/Main/Main.jsx
+++ primeiro-app/src/components/Main/Main.jsx
@@ -2,41 +2,26 @@ import Image from '../../images/imagem 1.webp'
 import Image2 from '../../images/imagem 2.jpg'
 import Image3 from '../../images/image 3.webp'
 import './Main.css'
-
+import Perfil from '../Perfil/Perfil'
 
 const Main = () => {
 
     return (
-        <>
+
             <main className='perfis'>
 
                 <h1 className='t-center'>DeadCells</h1>
 
-                <div className='t-center'>
+                <div>
 
-                    <img src={Image} />
-                    <h2>Wallpaper</h2>
-
-                </div>
-
-                <div className='t-center'>
-
-                    <img src={Image2} />
-                    <h2>Titulo</h2>
+                    <Perfil img={Image} titulo={'Wallpaper'} />
+                    <Perfil img={Image2} titulo={'Titulo'} />
+                    <Perfil img={Image3} titulo={'Personagem'} />
 
                 </div>
-
-                <div className='t-center'>
-
-                    <img src={Image3} />
-                    <h2>Personagem</h2>
-
-                </div>
-
 
             </main>
 
-        </>
     )
 
 }
