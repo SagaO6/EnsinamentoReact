d6a5ddc8a4dd90775fc9644bcfabc206f26a004e
THIAGO RIVAS CABALLERO <thiago.rcaballero@senacsp.edu.br>
2024-03-06 11:45:51 -0300

    Criada a pasta Descricao

--- primeiro-app/src/components/Descricao/Descricao.jsx
+++ primeiro-app/src/components/Descricao/Descricao.jsx
@@ -1,14 +1,24 @@
+import Image from '../../images/image 3.webp'
+import './Descricao.css'
+import Equipe from '../Equipe/Equipe'
+
 const Descricao = () => {
 
-return(
+    return (
+        <>
 
-<div>
+            
 
-<p>Teste de paragrafo</p>
+            <div className='text'>
+            <img src = {Image} />
+                <h1>Mude</h1>
+                <p>Eis aqui uma descrição que eu irei mudar quando chegar em casa. <br></br>Thiago altere essa merda logo.<br></br>-Thiago Rivas, 2024</p>
 
-</div>
+            </div>
 
-)
+        </>
+
+    )
 
 }
 
